5ecc5be4384d8e1ceb5fb16be6ed0f691c67fc8d
iLya2IK <medvedkovis@mail.ru>
2021-05-23 18:05:58 +0300

    tbufferedstream is ascending class for TCustomMemoryStream now

--- bufferedstream.pas
+++ bufferedstream.pas
@@ -15,75 +15,32 @@ type
 
 { TBufferedStream }
 
-TBufferedStream = class(TStream)
-private
-  FMemory: Pointer;
-  FSize, FPosition: PtrInt;
-protected
-  Function GetSize : Int64; Override;
-  function GetPosition: Int64; Override;
+TBufferedStream = class(TCustomMemoryStream)
 public
-  procedure SetPointer(Ptr: Pointer; ASize: PtrInt);
+  procedure SetPtr(Ptr: Pointer; ASize: PtrInt);
   function Write(const Buffer; Count: Longint): Longint; override;
-  function Read(var Buffer; Count: LongInt): LongInt; override;
-  function Seek(const Offset: Int64; Origin: TSeekOrigin): Int64; override;
-  property Memory: Pointer read FMemory;
 end;
 
 implementation
 
 { TBufferedStream }
 
-function TBufferedStream.GetSize: Int64;
+procedure TBufferedStream.SetPtr(Ptr: Pointer; ASize: PtrInt);
 begin
-  Result:=FSize;
-end;
-
-function TBufferedStream.GetPosition: Int64;
-begin
-  Result:=FPosition;
-end;
-
-procedure TBufferedStream.SetPointer(Ptr: Pointer; ASize: PtrInt);
-begin
-  FMemory:=Ptr;
-  FSize:=ASize;
-  FPosition:=0;
+  SetPointer(Ptr, ASize);
+  Seek(0, soFromBeginning);
 end;
 
 function TBufferedStream.Write(const Buffer; Count: Longint): Longint;
 begin
- Result:=Count;
- If (Count>0) then
-   begin
-     If (Result>(FSize-FPosition)) then
-       Result:=(FSize-FPosition);
-     Move (Buffer,(FMemory+FPosition)^,Result);
-     Inc(FPosition, Result);
-   end;
-end;
-
-function TBufferedStream.Read(var Buffer; Count: LongInt): LongInt;
-begin
- Result:=0;
- If (FSize>0) and (FPosition<Fsize) and (FPosition>=0) then
-   begin
-     Result:=Count;
-     If (Result>(FSize-FPosition)) then
-       Result:=(FSize-FPosition);
-     Move ((FMemory+FPosition)^,Buffer,Result);
-     Inc(FPosition, Result);
-   end;
-end;
-
-function TBufferedStream.Seek(const Offset: Int64; Origin: TSeekOrigin): Int64;
-begin
- Case Word(Origin) of
-   soFromBeginning : FPosition:=Offset;
-   soFromEnd       : FPosition:=FSize+Offset;
-   soFromCurrent   : FPosition:=FPosition+Offset;
- end;
- Result:=FPosition;
+  Result:=Count;
+  If (Count>0) then
+  begin
+    If (Result>(Size-Position)) then
+       Result:=(Size-Position);
+    Move(Buffer,(Memory+Position)^,Result);
+    Seek(Result, soFromCurrent);
+  end;
 end;
 
 end.
